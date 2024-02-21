b557f3e4c6d73eccf7668088fecbb5909e14538e
professorjrod <mumawjared@gmail.com>
2024-02-21 15:38:05 -0800

    No caching on old runtime with worldtime api(fixed)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import "@styles/globals.css";
+import { useEffect, useState } from "react";
 
-const time = () => fetch("http://worldtimeapi.org/api/ip");
+const getTime = async () => {
+  const response = await fetch("http://worldtimeapi.org/api/ip");
+  const data = await response.json(); // Parse JSON response
+  return data.datetime; // Return only the datetime property
+};
 
 function Application({ Component, pageProps }) {
-  return <p>{time()}</p>;
+  const [currentTime, setCurrentTime] = useState(null);
+
+  useEffect(() => {
+    const fetchTime = async () => {
+      const time = await getTime();
+      setCurrentTime(time);
+    };
+    fetchTime();
+  }, []);
+
+  return <p>{currentTime}</p>;
 }
 
 export default Application;
